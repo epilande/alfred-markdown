843526b4ca98056adcfc7e0ab2af2fbe65cb588f
Emmanuel Pilande <epilande@gmail.com>
2021-01-21 02:00:05 -0800

    :sparkles: Display active tab url as an option

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 clip0 = os.environ['clip0']
 clip1 = os.environ['clip1']
@@ -11,5 +12,21 @@ clip7 = os.environ['clip7']
 clip8 = os.environ['clip8']
 clip9 = os.environ['clip9']
 
-clipboard = filter(None, [clip0, clip1, clip2, clip3,
-                          clip4, clip5, clip6, clip7, clip8,  clip9])
+result = subprocess.run(['osascript', '-e',
+                         'if application "Google Chrome" is running then tell application "Google Chrome" to return URL of active tab of front window'
+                         ], stdout=subprocess.PIPE)
+activeTabUrl = result.stdout.decode('utf-8')
+
+clipboard = filter(None, [
+    activeTabUrl,
+    clip0,
+    clip1,
+    clip2,
+    clip3,
+    clip4,
+    clip5,
+    clip6,
+    clip7,
+    clip8,
+    clip9,
+])
